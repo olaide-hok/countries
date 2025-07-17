9f4a7093dae132b101d3ba078a50923596a3051f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-17 18:23:35 +0100

    add extra utility classes

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import {ModeToggle} from './ModeToggle';
 
 const Header = () => {
     return (
-        <header className="header-banner flex items-center justify-between bg-(--primary) lg:py-(--space-300) lg:px-(--space-1000)  max-w-[90rem] h-[5rem]">
-            <h1 className="text-(length:--fs-24) font-extrabold leading-(--lh-1375) ">
+        <header className="header-banner container mx-auto flex items-center justify-between bg-(--primary) px-(--space-200) py-[1.875rem] md:px-(--space-500) lg:py-(--space-300) lg:px-(--space-1000)   h-[5rem]">
+            <h1 className="text-(length:--fs-14) lg:text-(length:--fs-24) font-extrabold leading-[1.25rem] lg:leading-(--lh-1375)">
                 Where in the world?
             </h1>
             <ModeToggle />
