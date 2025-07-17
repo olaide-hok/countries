8858748034f717e4c5a3d7e7b331e67b16dc6996
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-17 18:27:39 +0100

    render BackBtn as a link tag

--- components/BackBtn.tsx
+++ components/BackBtn.tsx
@@ -2,15 +2,18 @@
 
 import {Button} from '@/components/ui/button';
 import {MoveLeft} from 'lucide-react';
+import Link from 'next/link';
 
 const BackBtn = () => {
     return (
         <Button
+            asChild
             variant="ghost"
-            className="back-btn bg-(--primary) text(length:--fs-16) leading-(--lh-200) font-light rounded-sm w-[8.5rem] h-(--space-500) cursor-pointer"
-            onClick={() => console.log('Back button clicked')}>
-            <MoveLeft />
-            Back
+            className="back-btn bg-(--primary) text(length:--fs-16) leading-(--lh-200) font-light rounded-sm w-[8.5rem] h-(--space-500) cursor-pointer">
+            <Link href="/">
+                <MoveLeft />
+                Back
+            </Link>
         </Button>
     );
 };
