0fdf8f4cbca3a3242361bec76cf7aee08a844417
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-17 18:29:14 +0100

    add formatNumber utility function to add commas to digits.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {clsx, type ClassValue} from 'clsx';
+import {twMerge} from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+// Format numbers with commas and 2 decimal places
+export const formatNumber = (num: number): string => {
+    return num.toLocaleString('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+};
