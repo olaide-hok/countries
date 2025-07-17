e04518bac38c5934b12bc4b9c18810c2a212a4a9
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-17 18:20:17 +0100

    add flagcdn.com to remotePatterns array

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [new URL('https://flagcdn.com/w320/**')],
+    },
 };
 
 export default nextConfig;
