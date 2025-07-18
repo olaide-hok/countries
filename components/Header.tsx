f3ac32e897d7a3ecaee589ec47c19f0f66c20d2a
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-18 10:22:07 +0100

    update utility classes

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import {ModeToggle} from './ModeToggle';
 
 const Header = () => {
     return (
-        <header className="header-banner container mx-auto flex items-center justify-between bg-(--primary) px-(--space-200) py-[1.875rem] md:px-(--space-500) lg:py-(--space-300) lg:px-(--space-1000)   h-[5rem]">
+        <header className="header-banner lg:container xl:mx-auto flex items-center justify-between bg-(--primary) px-(--space-200) py-[1.875rem] md:px-(--space-500) lg:py-(--space-300) lg:px-(--space-1000) h-[5rem]">
             <h1 className="text-(length:--fs-14) lg:text-(length:--fs-24) font-extrabold leading-[1.25rem] lg:leading-(--lh-1375)">
                 Where in the world?
             </h1>
