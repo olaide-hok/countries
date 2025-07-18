acb3a6955c30a70e98dc92b7c156361e1e0b2ce4
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-18 18:45:43 +0100

    add type and aria-label attribute

--- components/BackBtn.tsx
+++ components/BackBtn.tsx
@@ -8,6 +8,8 @@ const BackBtn = () => {
     return (
         <Button
             asChild
+            type="button"
+            aria-label="Back"
             variant="ghost"
             className="back-btn bg-(--primary) text(length:--fs-16) leading-(--lh-200) font-light rounded-sm w-[8.5rem] h-(--space-500) cursor-pointer">
             <Link href="/">
