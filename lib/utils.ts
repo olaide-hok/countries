4e0a7c1529776f858b6c62aac29c0722b71b8d7b
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-18 10:21:07 +0100

    remove minimumFractionDigits

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,10 +5,9 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-// Format numbers with commas and 2 decimal places
+// Format numbers with commas
 export const formatNumber = (num: number): string => {
     return num.toLocaleString('en-US', {
-        minimumFractionDigits: 2,
         maximumFractionDigits: 2,
     });
 };
