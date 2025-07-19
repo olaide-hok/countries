0a9bad4f1626ca99567acddac0c23886a60b43f0
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-19 16:16:51 +0100

    create CountryCardAPI component to match API return data shape.

---
+++ components/CountryCardAPI.tsx
@@ -0,0 +1,56 @@
+import {formatNumber} from '@/lib/utils';
+import {CountryAPI} from '@/types/country';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface CountryCardAPIProps {
+    country: CountryAPI;
+}
+
+const CountryCardAPI = ({country}: CountryCardAPIProps) => {
+    const {name, population, region, capital, flags} = country;
+
+    return (
+        <Link
+            href={`/country/${name.common}`}
+            className="country-card bg-(--primary) flex flex-col rounded-[0.3125rem] w-[16.5rem] h-[21rem] overflow-hidden">
+            {/* Flag */}
+            <div className="flag flex-1 overflow-hidden">
+                <Image
+                    src={flags.svg}
+                    alt={flags.alt}
+                    width={267}
+                    height={160}
+                />
+            </div>
+
+            {/* Details */}
+            <div className="details flex flex-col flex-1 gap-y-(--space-200) pl-[1.62rem] pt-[1.37rem]">
+                {/* Name */}
+                <h2 className="text-(length:--fs-18) font-extrabold leading-(--lh-145)">
+                    {name.common}
+                </h2>
+
+                <div className="flex flex-col gap-y-(--space-100)">
+                    {/* Population */}
+                    <p className="text-(length:--fs-14) font-semibold leading-[1rem]">
+                        Population:{' '}
+                        <span className="font-light">
+                            {formatNumber(population)}
+                        </span>
+                    </p>
+                    {/* Region */}
+                    <p className="text-(length:--fs-14) font-semibold leading-[1rem]">
+                        Region: <span className="font-light">{region}</span>
+                    </p>
+                    {/* Capital */}
+                    <p className="text-(length:--fs-14) font-semibold leading-[1rem]">
+                        Capital: <span className="font-light">{capital}</span>
+                    </p>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default CountryCardAPI;
